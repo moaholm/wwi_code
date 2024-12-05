c920f4783decec02eb722a41da39d8024d9bb159
moaholm <mkeholmberg@gmail.com>
2024-12-05 09:37:36 +0100

    add external table customerCatebory in BigQuarey

--- tables/customercategories.sql
+++ tables/customercategories.sql
@@ -1,8 +1,17 @@
-CREATE TABLE `data-evolution-moa.raw_wwi.customerCategory`(
+CREATE OR REPLACE EXTERNAL TABLE `data-evolution-moa.raw_wwi.customerCategory`(
     CustomerCategoryID STRING,
     CustomerCategoryName STRING,
     LastEditedBy STRING,
-    ValidFro STRING,
+    ValidFrom STRING,
     ValidTo STRING
 )
-PARTITIONED BY (year STRING, month STRING)
+WITH PARTITION COLUMNS (
+    year STRING,
+    month STRING
+)
+OPTIONS (
+    format = 'CSV',
+    uris = ['gs://raw-wwi-moa/data-evolution-wwi/csv/sales.customercategories/*.csv'],
+    hive_partition_uri_prefix = 'gs://raw-wwi-moa/data-evolution-wwi/csv/sales.customercategories/',
+    require_hive_partition_filter = false
+    )
