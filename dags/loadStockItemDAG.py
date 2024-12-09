e518cc6eefddfa93e89f461393752e79f7ee7ba5
moaholm <mkeholmberg@gmail.com>
2024-12-09 15:16:40 +0100

    a WIP, maybe working, AirFlow is takeing soooo long to refresh...

--- dags/loadStockItemDAG.py
+++ dags/loadStockItemDAG.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 default_args = {
@@ -8,50 +10,88 @@ default_args = {
     'retries': 1,
 }
 
-dag = DAG(
-    'gcs_to_bigquery',
-    default_args = default_args,
-    schedule_interval = None
-)
+with DAG(
+    dag_id='load_stock_items_to_bg',
+    default_args= default_args,
+    schedule_interval= None
+) as dag:
+    
+    gcs_file_path = 'data-evolution-wwi/csv/warehouse.stockitem/year=2013/month=99/warehouse.stockitem_201399.csv'
 
-load_csv = GCSToBigQueryOperator(
-    task_id = 'load_stock_groups',
-    bucket = 'raw-wwi-moa',
-    source_objects = ['data-evolution-wwi/csv/warehouse.stockgroups/year=2013/month=99/warehouse.stockgroups_201399.csv'],
-    destination_project_dataset_table = 'data-evolution-moa.raw_wwi.stockGroups',
-    schema_fields = [
-        {'name' : 'stockItemID', 'type' : 'STRING'},
-        {'name' : 'stockItemName', 'type' : 'STRING'},
-        {'name' : 'supplierID', 'type' : 'STRING'},
-        {'name' : 'colorID', 'type' : 'STRING'},
-        {'name' : 'unitPackageID', 'type' : 'STRING'}
-        {'name' : 'outerPackageID', 'type' : 'STRING'}
-        {'name' : 'brand', 'type' : 'STRING'}
-        {'name' : 'size', 'type' : 'STRING'}
-        {'name' : 'leadTimeDays', 'type' : 'STRING'}
-        {'name' : 'quantityPerOuter', 'type' : 'STRING'}
-        {'name' : 'isChillerStock', 'type' : 'STRING'}
-        {'name' : 'barcode', 'type' : 'STRING'}
-        {'name' : 'taxRate', 'type' : 'STRING'}
-        {'name' : 'unitPrice', 'type' : 'STRING'}
-        {'name' : 'unitPrice', 'type' : 'STRING'}
-    ],
-    write_disposition = 'WRITE_TRUNCATE',
-    dag = dag
-)
+    def extract_year_month(file_path):
+        parts = file_path.split('/')
+        year = parts[-3].split('=')[1]
+        month = parts[-2].split('=')[1]
+        return year, month
 
-load_csv
+    def extract_year_month_task(**context):
+        year, month = extract_year_month(gcs_file_path)
+        context['ti'].xcom_push(key='year', value=year)
+        context['ti'].xcom_push(key='month', value = month)
 
-    recommendedRetailPrice STRING,
-    typicalWeightPerUnit STRING,
-    marketingComments STRING,
-    internalComments STRING,
-    photo STRING,
-    customFields STRING,
-    tags STRING,
-    searchDetails STRING,
-    lastEditedBy STRING,
-    validFrom STRING,
-    validTo STRING,
-    year STRING,
-    month STRING
+    extract_year_month_operator = PythonOperator(
+        task_id = 'extract_year_month',
+        python_callable= extract_year_month_task,
+        provide_context = True
+    )
+
+    load_to_temp_table_stockitems = GCSToBigQueryOperator(
+        task_id = 'load_to_temp_table_stockitems',
+        bucket = 'raw-wwi-moa',
+        source_objects = [gcs_file_path],
+        destination_project_dataset_table = 'data-evolution-moa.raw_wwi.temp_warehouse_stockitems',
+        schema_fields = [
+            {'name' : 'stockItemID', 'type' : 'STRING'},
+            {'name' : 'stockItemName', 'type' : 'STRING'},
+            {'name' : 'supplierID', 'type' : 'STRING'},
+            {'name' : 'colorID', 'type' : 'STRING'},
+            {'name' : 'unitPackageID', 'type' : 'STRING'},
+            {'name' : 'outerPackageID', 'type' : 'STRING'},
+            {'name' : 'brand', 'type' : 'STRING'},
+            {'name' : 'size', 'type' : 'STRING'},
+            {'name' : 'leadTimeDays', 'type' : 'STRING'},
+            {'name' : 'quantityPerOuter', 'type' : 'STRING'},
+            {'name' : 'isChillerStock', 'type' : 'STRING'},
+            {'name' : 'barcode', 'type' : 'STRING'},
+            {'name' : 'taxRate', 'type' : 'STRING'},
+            {'name' : 'unitPrice', 'type' : 'STRING'},
+            {'name' : 'recommendedRetailPrice', 'type' : 'STRING'},
+            {'name' : 'typicalWeightPerUnit', 'type' : 'STRING'},
+            {'name' : 'marketingComments', 'type' : 'STRING'},
+            {'name' : 'internalComments', 'type' : 'STRING'},
+            {'name' : 'photo', 'type' : 'STRING'},
+            {'name' : 'customFields', 'type' : 'STRING'},
+            {'name' : 'tags', 'type' : 'STRING'},
+            {'name' : 'searchDetails', 'type' : 'STRING'},
+            {'name' : 'lastEditedBy', 'type' : 'STRING'},
+            {'name' : 'validFrom', 'type' : 'STRING'},
+            {'name' : 'validTo', 'type' : 'STRING'}],
+        source_format = 'CSV',
+        skip_leading_rows = 1,
+        write_disposition = 'WRITE_TRUNCATE',
+        autodetect = False
+    )
+
+    year_placeholder = '{{ ti.xcom_pull(task_ids="extract_year_month", key="year")}}'
+    month_placeholder = '{{ ti.xcom_pull(task_ids="extract_year_month", key="month")}}'
+
+    merge_stockitems_to_final_table = BigQueryInsertJobOperator(
+        task_id = 'merge_stockitems_to_final_table',
+        configuration = {
+            "query": {
+                "query": f"""
+                    INSERT INTO `data-evolution-moa.raw_wwi.stockItem` (
+                        stockGroupID, stockGroupName, lastEditedBy, year, month
+                    )
+                    SELECT
+                        stockGroupID, stockGroupName, lastEditedBy,
+                        '{year_placeholder}' AS year,
+                        '{month_placeholder}' AS month
+                    FROM `data-evolution-moa.raw_wwi.temp_warehouse_stockitems`
+                """,
+                "useLegacySql": False,
+            }
+        },
+    )
+
+    extract_year_month_operator >> load_to_temp_table_stockitems >> merge_stockitems_to_final_table
