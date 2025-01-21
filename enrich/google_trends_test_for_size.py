c5452b2976276b7d5934e275a4eb8c69cc6000fa
moaholm <mkeholmberg@gmail.com>
2025-01-21 10:14:50 +0100

    a version that has worked as test from time to time. Now trying to upload to BQ

--- enrich/google_trends_test_for_size.py
+++ enrich/google_trends_test_for_size.py
@@ -1,13 +1,24 @@
 from pytrends.request import TrendReq
+from google.cloud import bigquery
+import pandas as pd
+import os
 import time
 
 pytrends = TrendReq()
-keywords = ["bubblewrap", "packing materials"]
+bq_client = bigquery.Client()
 
-pytrends.build_payload(keywords, cat=0, timeframe='2013-01-01 2013-01-05')
+keywords = ["bubble wrap", "packing materials"]
+
+pytrends.build_payload(keywords, cat=0, timeframe='2013-01-01 2016-05-31')
 time.sleep(60)
 trend_data = pytrends.interest_over_time()
 
-print(trend_data.head())
-print(f"Antal rader: {trend_data.shape[0]}")
-print(f"Antal kolumner: {trend_data.shape[1]}")
+trend_data = trend_data.drop(columns=['isPartial'])
+trend_data = trend_data.reset_index()
+trend_data = trend_data.melt(id_vars=["date"], var_name="keyword", value_name="trend_score")
+
+table_id = "data-evolution-moa.raw_wwi.google_trends_data"
+
+job = bq_client.load_table_from_dataframe(trend_data, table_id)
+job.result()
+print(f"Data har laddats upp till {table_id}")
